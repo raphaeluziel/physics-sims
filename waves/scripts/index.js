0bf1d153d3872214ee318fe4e83d1d9924e12992
Raphael Uziel <raphaeluziel@hotmail.com>
2020-01-24 15:00:45 -0500

    back to atom

--- waves/scripts/index.js
+++ waves/scripts/index.js
@@ -27,19 +27,35 @@ document.addEventListener('DOMContentLoaded', function(){
     objects.camera = this.cameras.add(0, 0, 800, 480);
     objects.camera.setBackgroundColor('rgba(255, 255, 255, 0.95)');
 
+    graphics = this.add.graphics();
+
+    graphics.lineStyle(5, 0xFF00FF, 1.0);
+    graphics.beginPath();
+    graphics.moveTo(100, 100);
+    graphics.lineTo(200, 200);
+    graphics.closePath();
+    graphics.strokePath();
+
+
     particles = [];
 
-    for (var i = 0; i < 3200; ++i){
-      particles[i] = this.add.sprite(i/4, 240 + 40 * Math.sin(i/200), 'particle').setScale(0.02);
+    for (var i = 0; i < 40; ++i){
+      particles[i] = this.add.sprite(32 * i, 240 + 40 * Math.sin(i/200), 'particle').setScale(0.02);
     }
 
   }
 
   function update (time, delta)
   {
-    for (var i = 0; i < 3200; i++){
-      particles[i].y = 240 - 100 * Math.sin((time / 5000 - particles[i].x / 50));
-    }
+    for (var i = 0; i < 40; i++){
+        particles[i].y = 240 - 100 * Math.sin((time / 5000 - particles[i].x / 40));
+        graphics.lineStyle(5, 0xFF00FF, 1.0);
+        graphics.beginPath();
+        graphics.moveTo(100, 100);
+        graphics.lineTo(200, 200);
+        graphics.closePath();
+        graphics.strokePath();
+        }
   }
 
 });
