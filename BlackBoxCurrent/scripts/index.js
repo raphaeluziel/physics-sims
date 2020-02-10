7cc27f9871582fcda85dd0ef21500e4a482d79a0
unknown <raphaeluziel@hotmail.com>
2020-02-10 12:47:21 -0500

    added firing mechanism

--- BlackBoxCurrent/scripts/index.js
+++ BlackBoxCurrent/scripts/index.js
@@ -17,29 +17,36 @@ document.addEventListener('DOMContentLoaded', function(){
   };
 
   var game = new Phaser.Game(config);
+  var cursors;
+  var gun;
+  var shapes;
+  var lastFired = 0;
 
   function preload ()
   {
     this.load.atlas('sheet', 'assets/blackbox.png', 'assets/blackbox.json');
     this.load.json('shapes', 'assets/blackboxPhysics.json');
+    this.load.image('gun', 'sprites/gun.png');
     this.load.image('line', 'sprites/line.png');
   }
 
   function create ()
   {
     this.matter.world.disableGravity();
+    cursors = this.input.keyboard.createCursorKeys();
+
+    gun = this.add.sprite(200, 300, 'gun').setScale(0.4);
 
     this.add.image(400, 300, 'line').setScale(1, 0.4);
     line = this.add.image(600, 300, 'line').setScale(1, 0.4);
     line.angle = 90;
 
+    shapes = this.cache.json.get('shapes');
 
-    var shapes = this.cache.json.get('shapes');
-
-    particle = this.matter.add.sprite(200, 230, 'sheet', 'particle', {shape: shapes.particle}).setScale(0.2);
+    particle = this.matter.add.sprite(200, 230, 'sheet', 'particle', {shape: shapes.particle}).setScale(0.05);
     particle.setVelocity(6, 0);
 
-    sha05 = this.matter.add.sprite(600, 300, 'sheet', 'sha05', {shape: shapes.sha05});
+    sha06 = this.matter.add.sprite(600, 300, 'sheet', 'sha06', {shape: shapes.sha06});
 
     this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
         console.log(event, bodyA, bodyB);
@@ -49,9 +56,29 @@ document.addEventListener('DOMContentLoaded', function(){
 
   }
 
-  function update (t)
+  function update (time)
   {
+      if (cursors.up.isDown)
+      {
+        console.log("moving");
+        gun.y += -2;
+      }
 
+      else if (cursors.down.isDown)
+      {
+          gun.y += 2;;
+      }
+
+      if (cursors.space.isDown)
+      {
+        gun.angle += -2;
+      }
+      if (cursors.space.isDown && time > lastFired)
+      {
+        particle = this.matter.add.sprite(200, 230, 'sheet', 'particle', {shape: shapes.particle}).setScale(0.05);
+        particle.setVelocity(6 * Math.cos(gun.angle), 6 * Math.sin(gun.angle));
+        lastFired = time + 50;
+      }
   }
 
 });
