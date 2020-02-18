adb5c3f7d06172aa669e09c608a2ed5a6a2b5c12
Raphael Uziel <raphaeluziel@hotmail.com>
2020-02-18 16:00:59 -0500

    getting closer

--- BlackBox/scripts/index.js
+++ BlackBox/scripts/index.js
@@ -19,22 +19,28 @@ document.addEventListener('DOMContentLoaded', function(){
   var game = new Phaser.Game(config);
   var cursors;
   var gun;
-  var shapes;
+  var shapes; // object of objects of shapes from physics editor
+  var shape = []; shape[0] = null; // array of shapes for display, from texture packer
+  var buttons = []; buttons[0] = null; var n = 8;
   var particles = [];
   var lastFired = 0;
   var v = 5;
   const Y = 300; // the equilibrium position
-  var n = 8;
+  var activeShapeNumber = 1;
 
   function preload ()
   {
     this.load.atlas('sheet', 'assets/blackbox.png', 'assets/blackbox.json');
     this.load.json('shapes', 'assets/blackboxPhysics.json');
     this.load.image('gun', 'sprites/gun.png');
-    this.load.image('button6', 'sprites/button6.png');
-    this.load.image('line', 'sprites/line.png');
+    //this.load.image('line', 'sprites/line.png');
     this.load.image('box', 'sprites/box.png');
-    this.load.image('circleguide', 'sprites/circleguide.png');
+    this.load.image('blackbox', 'sprites/blackbox.png');
+    //this.load.image('circleguide', 'sprites/circleguide.png');
+    for (let n = 1; n < 11; n++)
+    {
+      this.load.image('button' + n, 'buttons/button' + n + '.png');
+    }
   }
 
   function create ()
@@ -44,32 +50,43 @@ document.addEventListener('DOMContentLoaded', function(){
 
     gun = this.add.sprite(140, Y + 9, 'gun').setScale(0.4);
 
-    this.add.image(400, 300, 'line').setScale(1, 0.4);
-    line = this.add.image(600, 300, 'line').setScale(1, 0.4);
-    line.angle = 90;
-
-    this.add.image(260, 300, 'circleguide');
-
+    //this.add.image(400, 300, 'line').setScale(1, 0.4);
+    //line = this.add.image(600, 300, 'line').setScale(1, 0.4);
+    //line.angle = 90;
+    //this.add.image(260, 300, 'circleguide');
     this.add.image(600, 300, 'box');
+
 
     shapes = this.cache.json.get('shapes');
 
-    shape8 = this.matter.add.sprite(600, 300, 'sheet', 'shape' + n, {shape: shapes["shape" + n]});
-    //shape1.setPosition(638, 338);
-    // DONT THINK YOU SHOULD USE THIS ONE shape7.setPosition(565, 300);
-    shape8.setPosition(508, 298);
-    //shape9.setPosition(638, 262);
+    for (let n = 1; n < 11; n++)
+    {
+        // Shapes are all added, but placed outside of the world
+        shape[n] = this.matter.add.sprite(-1000, -1000, 'sheet', 'shape' + n, {shape: shapes["shape" + n]});
+    }
 
-    var button6 = this.add.sprite(400, 550, 'button6').setScale(0.3).setInteractive();
+    // Shape1 is the example.
+    shape[1].setPosition(638, 338);
 
-    button6.on('pointerdown', function (pointer) {
-      //shape0.setVisible(true);
-    });
+    for (let n = 1; n < 11; n++)
+    {
+        buttons[n] = this.add.sprite(100 + 48*n, 580, 'button' + n).setScale(0.3).setInteractive();
+        buttons[n].on('pointerdown', function (pointer) {
+          activeShapeNumber = n;
+          for (let m = 1; m < 11; m++)
+            m == n ? shape[m].setPosition(600, 300) : shape[m].setPosition(-1000, -1000);
+          if (n == 1) shape[n].setPosition(638, 338);
+          if (n == 8) shape[n].setPosition(508, 298);
+          if (n == 9) shape[n].setPosition(638, 262);
+        });
+    }
 
     this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
-        //if (shape0.visible)
-          bounce8(bodyB.parent.gameObject);
+        bounce[activeShapeNumber - 1](bodyB.parent.gameObject);
     });
+
+    // image of blackbox must be at end to cover all other shapes!
+    //this.add.image(600, 300, 'blackbox');
 
   }
 
@@ -96,91 +113,93 @@ document.addEventListener('DOMContentLoaded', function(){
         }
   }
 
-  function bounce1 (particle) // right triangle up
-  {
-    particle.setVelocity(0, -v);
-  }
-  function bounce2 (particle) // large circle
-  {
-      let y = 300 - particle.y;
-      let r = 112;
-      if (Math.abs(y) <= r){
-        let o = Math.asin(y / r);
-        let vx = - v * Math.cos(2 * o);
-        let vy = - v * Math.sin(2 * o);
-        particle.setVelocity(vx, vy);
-      }
-  }
-  function bounce3 (particle) // trapezoid
-  {
-      if (particle.y < Y - 40)
-        particle.setVelocity(0, -v);
-      else if (particle.y > Y + 40)
-        particle.setVelocity(0, v);
-      else
-        particle.setVelocity(-v, 0);
-  }
-  function bounce4 (particle) // large square
-  {
-      particle.setVelocity(-v, 0);
-  }
-  function bounce5 (particle) // small circle
-  {
-      let y = 300 - particle.y;  console.log(y);
-      let r = 52;
-      if (Math.abs(y) <= r){
-        let o = Math.asin(y / r);
-        let vx = - v * Math.cos(2 * o);
-        let vy = - v * Math.sin(2 * o);
-        particle.setVelocity(vx, vy);
-      }
-  }
-  function bounce6 (particle) // isosceles triangle
-  {
-      if (particle.y < Y)
-        particle.setVelocity(0, -v);
-      else if (particle.y > Y)
-        particle.setVelocity(0, v);
-      else
-        particle.setVelocity(-v, 0);
-  }
   let secondHit = false;
-  function bounce7 (particle) // moon mirror
-  {
-      if (particle.y < Y && !secondHit)
+  const bounce = [
+      function (particle) // right triangle up
       {
-          particle.setVelocity(0, v);
-          secondHit = true;
-      }
-      else if (particle.y > Y && !secondHit)
+        particle.setVelocity(0, -v);
+      },
+      function (particle) // large circle
       {
-          particle.setVelocity(0, -v);
-          secondHit = true;
-      }
-      else
+          let y = 300 - particle.y;
+          let r = 112;
+          if (Math.abs(y) <= r){
+            let o = Math.asin(y / r);
+            let vx = - v * Math.cos(2 * o);
+            let vy = - v * Math.sin(2 * o);
+            particle.setVelocity(vx, vy);
+          }
+      },
+      function (particle) // trapezoid
+      {
+          if (particle.y < Y - 40)
+            particle.setVelocity(0, -v);
+          else if (particle.y > Y + 40)
+            particle.setVelocity(0, v);
+          else
+            particle.setVelocity(-v, 0);
+      },
+      function (particle) // large square
       {
           particle.setVelocity(-v, 0);
-          secondHit = false;
+      },
+      function (particle) // small circle
+      {
+          let y = 300 - particle.y;  console.log(y);
+          let r = 52;
+          if (Math.abs(y) <= r){
+            let o = Math.asin(y / r);
+            let vx = - v * Math.cos(2 * o);
+            let vy = - v * Math.sin(2 * o);
+            particle.setVelocity(vx, vy);
+          }
+      },
+      function (particle) // isosceles triangle
+      {
+          if (particle.y < Y)
+            particle.setVelocity(0, -v);
+          else if (particle.y > Y)
+            particle.setVelocity(0, v);
+          else
+            particle.setVelocity(-v, 0);
+      },
+      function (particle) // moon mirror
+      {
+          if (particle.y < Y && !secondHit)
+          {
+              particle.setVelocity(0, v);
+              secondHit = true;
+          }
+          else if (particle.y > Y && !secondHit)
+          {
+              particle.setVelocity(0, -v);
+              secondHit = true;
+          }
+          else
+          {
+              particle.setVelocity(-v, 0);
+              secondHit = false;
+          }
+      },
+      function (particle) // concave mirror
+      {
+          let y = 300 - particle.y;
+          let r = 240;
+          if (Math.abs(y) <= r - 135){
+            let o = Math.asin(y / r);
+            let vx = - v * Math.cos(2 * o);
+            let vy = v * Math.sin(2 * o);
+            particle.setVelocity(vx, vy);
+        }
+      },
+      function (particle) // right triangle down
+      {
+          particle.setVelocity(0, v);
+      },
+      function (particle) // small square
+      {
+          particle.setVelocity(-v, 0);
       }
-  }
-  function bounce8 (particle) // concave mirror
-  {
-      let y = 300 - particle.y;
-      let r = 240;
-      if (Math.abs(y) <= r - 135){
-        let o = Math.asin(y / r);
-        let vx = - v * Math.cos(2 * o);
-        let vy = v * Math.sin(2 * o);
-        particle.setVelocity(vx, vy);
-    }
-  }
-  function bounce9 (particle) // right triangle down
-  {
-      particle.setVelocity(0, v);
-  }
-  function bounce10 (particle) // small square
-  {
-      particle.setVelocity(-v, 0);
-  }
+    ]
 
 });
